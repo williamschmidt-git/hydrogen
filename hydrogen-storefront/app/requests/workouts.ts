708e1dec38e433bc87532e4f7141a691769da117
williamschmidt-git <9williamschmidt@gmail.com>
2023-06-11 22:06:08 -0300

    teste

--- hydrogen-storefront/app/requests/workouts.ts
+++
@@ -1,8 +0,0 @@
-export const workoutRequests = async () => {
-  const response = await fetch('http://localhost:3001/workout', {
-    method: 'GET',
-    headers: {'Content-type': 'application/json; charset=UTF-8'},
-  });
-
-  return response.json();
-};
