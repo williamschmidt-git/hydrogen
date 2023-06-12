63022f4f669575298349706dc5fa4a4b331f758f
williamschmidt-git <9williamschmidt@gmail.com>
2023-06-11 22:51:16 -0300

    fixed

--- hydrogen-storefront/app/routes/_index.tsx
+++ hydrogen-storefront/app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import {useLoaderData} from '@remix-run/react';
+import LoginIndexPage from './Login._index';
 
 export function meta() {
   return [
@@ -7,36 +8,6 @@ export function meta() {
   ];
 }
 
-export async function loader({context}) {
-  return await context.storefront.query(COLLECTIONS_QUERY);
-  // const response = await fetch('http://localhost:3001/user/create', {
-  //   method: 'POST',
-  //   // eslint-disable-next-line @typescript-eslint/naming-convention
-  //   headers: {'Content-type': 'application/json; charset=UTF-8'},
-  //   body: JSON.stringify({
-  //     username: 'username',
-  //     password: 'password',
-  //   }),
-  // });
-  // console.log(response);
-  // return null;
-}
-
 export default function Index() {
-  const {collections}: any = useLoaderData();
-  console.log(collections);
-
-  return <p>Hello from the home page!</p>;
+  return <LoginIndexPage />;
 }
-
-const COLLECTIONS_QUERY = `#graphql
-  query FeaturedCollections {
-    collections(first: 3, query: "collection_type:smart") {
-      nodes {
-        id
-        title
-        handle
-      }
-    }
-  }
-`;
